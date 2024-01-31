4b3615f31913b02a33f04851817c417dae8f8020
svoma <vomasrikanth5209@gmail.com>
2024-02-01 00:43:56 +0530

    homepage url fix and cleanup

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@
 */
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import { HashRouter, Route, Navigate, Routes } from "react-router-dom";
 
 // styles
 import "bootstrap/scss/bootstrap.scss";
@@ -32,11 +32,11 @@ import NucleoIcons from "views/NucleoIcons.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter basename="/">
+  <HashRouter>
     <Routes>
       <Route path="/index" element={<Index />} />
       <Route path="/nucleo-icons" element={<NucleoIcons />} />
       <Route path="*" element={<Navigate to="/index" replace />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
